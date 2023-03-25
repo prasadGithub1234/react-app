396eeafdaa01a400f52a0a2f28c1f3487e4e3f97
kaduprasad <kaduprasad99@gmail.com>
2023-03-25 23:22:26 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          update 2
+          update 3
         </p>
         <a
           className="App-link"
