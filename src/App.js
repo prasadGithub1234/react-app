a2adac55f77a7a82edde74042409911554b34923
prasadGithub1234 <kaduprasad936@gmail.com>
2023-03-27 02:03:42 +0530

    code update for paragraph

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is react app for new-feature</p>
+        <p>This is react app for new-feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
