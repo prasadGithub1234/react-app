2a739f1dffed43765940709a444199bf65e21b19
prasadGithub1234 <kaduprasad936@gmail.com>
2023-03-27 01:50:09 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is new feature-branch
-        </p>
-        <p>
-          commit 2
-        </p>
+        <p>This is react app</p>
         <a
           className="App-link"
           href="https://reactjs.org"
